061295b10e78721cc1c87215d7b57b897501988d
Adam Driggers <awdriggs@gmail.com>
2024-10-04 12:09:50 +0200

    update

--- species/single-species.js
+++ species/single-species.js
@@ -1,16 +1,45 @@
 console.log("working yo");
 
+var Airtable = require('airtable');
+var base = new Airtable({ apiKey: 'patxeF8i4nhZP07q7.c5859ae986f18ab5b26786adee657598d3110ae493d921a44062fcc628e873c7' }).base('appkKVYgOCUz8gC1H');
+
 // Get the query string from the URL (the part after ?)
 let queryString = window.location.search;
-
 // Create a URLSearchParams object
 let urlParams = new URLSearchParams(queryString);
 
 // Get individual parameters by name
-let species = urlParams.get('id'); 
+let id = urlParams.get('id');
 
-console.log(species);  // Output: "johnDoe"
+// console.log(species);  // Output: "johnDoe"
 
-let title = document.querySelector(".species-name");
+// let title = document.querySelector(".species-name");
+// title.innerHTML = species
 
-title.innerHTML = species
+//get data from airtable
+base('species').find(id, function(err, record) {
+  if (err) { console.error(err); return; }
+
+  // console.log('Retrieved', record.id);
+  // debugger;
+
+  let container = document.querySelector("#species-wrapper");
+
+
+
+  let title = document.createElement("h2");
+  title.classList.add("callout");
+  title.innerHTML = record.fields.name;
+  container.append(title);
+  //make a title
+  //put the photo in
+  //put the desc in
+
+  let img = document.createElement("img");
+  img.src = record.fields.canonical_img[0].url;
+  container.append(img);
+
+  let desc = document.createElement("p");
+  desc.innerHTML = record.fields.description;
+  container.append(desc);
+});
---
+++ species/species.js
@@ -0,0 +1,51 @@
+var Airtable = require('airtable');
+// Get a base ID for an instance of art gallery example
+var base = new Airtable({ apiKey: 'patxeF8i4nhZP07q7.c5859ae986f18ab5b26786adee657598d3110ae493d921a44062fcc628e873c7' }).base('appkKVYgOCUz8gC1H');
+
+
+base('species').select({
+  // Selecting the first 3 records in Grid view:
+  // maxRecords: 3,
+  view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+  // This function (`page`) will get called for each page of records.
+  let container = document.querySelector("#species-wrapper");
+
+  records.forEach(function(record) {
+    // console.log('Retrieved', record.get('slug'));
+    // debugger;
+    let anchor = document.createElement("a");
+    let baseUrl = window.location.origin;
+    let param = record.id;
+    let targetUrl =`${baseUrl}/species/single.html?id=${encodeURIComponent(param)}`;
+
+    anchor.href = targetUrl;
+
+
+    let card = document.createElement("section");
+    card.classList.add("card");
+
+    let title = document.createElement("h2");
+    title.classList.add("callout");
+    title.innerHTML = record.fields.name;
+    card.append(title);
+
+    let img = document.createElement("img");
+    img.src = record.fields.canonical_img[0].url;
+    card.append(img);
+
+
+    anchor.append(card);
+    container.append(anchor);
+  });
+
+  // To fetch the next page of records, call `fetchNextPage`.
+  // If there are more records, `page` will get called again.
+  // If there are no more records, `done` will get called.
+  fetchNextPage();
+
+}, function done(err) {
+  if (err) { console.error(err); return; }
+});
+
+
