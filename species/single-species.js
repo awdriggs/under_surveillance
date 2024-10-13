e8a73e67bea749d59fce2a5d51e6149a9ede6571
Adam Driggers <awdriggs@gmail.com>
2024-10-13 16:59:30 +0200

    update

--- species/single-species.js
+++ species/single-species.js
@@ -3,6 +3,10 @@ console.log("working yo");
 var Airtable = require('airtable');
 var base = new Airtable({ apiKey: 'patxeF8i4nhZP07q7.c5859ae986f18ab5b26786adee657598d3110ae493d921a44062fcc628e873c7' }).base('appkKVYgOCUz8gC1H');
 
+//used for sightings
+var reverseGeocoder = new BDCReverseGeocode();
+
+//start building the page
 // Get the query string from the URL (the part after ?)
 let queryString = window.location.search;
 // Create a URLSearchParams object
@@ -23,12 +27,16 @@ base('species').find(id, function(err, record) {
   // console.log('Retrieved', record.id);
   // debugger;
 
-  let container = document.querySelector("#species-wrapper");
+  let container = document.querySelector("#info-wrapper");
 
   let title = document.createElement("h2");
   title.classList.add("callout");
   title.innerHTML = record.fields.name;
   container.append(title);
+
+  //get recent sightings, async
+  getRecentSightings(record.fields.slug);
+
   //make a title
   //put the photo in
   //put the desc in
@@ -72,3 +80,80 @@ base('species').find(id, function(err, record) {
   desc.innerHTML = record.fields.description;
   container.append(desc);
 });
+
+function getRecentSightings(slug){
+  console.log("slug", slug);
+  let sightings = [];
+  base('sightings').select({
+    // Selecting the first 3 records in Grid view:
+    maxRecords: 3,
+    view: "Grid view",
+    filterByFormula: `{species} = "${slug}"`,
+
+  }).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+    records.forEach(function(record) {
+      sightings.push(record);
+      console.log('Retrieved', record.get('timestamp'));
+    });
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    fetchNextPage();
+
+  }, function done(err) {
+    if (err) { console.error(err); return; }
+    // var geocoder = require('geocoder');
+    let sightingsWrapper = document.querySelector("#recent-sightings");
+
+    for(let s of sightings){
+      let card = document.createElement("div");
+      card.classList.add("card");
+
+      let img = document.createElement("img");
+      img.src = s.get("base64");
+      card.append(img);
+
+      let info = document.createElement("div");
+      info.classList.add("sighting-info");
+       
+      let location = document.createElement("p");
+      getLocation(location, s.get("latitude"), s.get("longitude"));
+      location.classList.add("location", "callout");
+      info.append(location);
+    
+
+      let date = document.createElement("p");
+      console.log(s.get("timestamp"));
+      date.innerHTML = convertTimestamp(s.get("timestamp"));
+      info.append(date);
+
+      card.append(info);
+
+      sightingsWrapper.append(card);
+    }
+    console.log(sightings);
+  });
+}
+
+function getLocation(el, lat, long){
+  reverseGeocoder.getClientLocation({
+    latitude: Number(lat),
+    longitude: Number(long),
+  },function(result) {
+    console.log(result);
+    // debugger;
+    let loc = `${result.city}, ${result.countryCode}`
+    el.innerHTML = loc; //set the html to the lcoation 
+  });
+}
+
+
+function convertTimestamp(t){
+  // console.log(Number(t));
+  let timestamp = Number(t);
+  let date = new Date(timestamp);
+  return date.toLocaleString();
+}
